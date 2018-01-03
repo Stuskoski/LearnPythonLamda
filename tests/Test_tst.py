e6a3e8d38f5267b432d622434e9109e8d51e9e5a
StuskoskiHEB <rutkoski.augustus@heb.com>
2018-01-03 11:14:00 -0600

    Added failing test

--- tests/Test_tst.py
+++ tests/Test_tst.py
@@ -6,6 +6,9 @@ class MyTestCase(unittest.TestCase):
     def test_default_greeting_set(self):
         self.assertEqual(2, (1 + 1))
 
+    def test_fail(self):
+        self.assertTrue('foo'.isupper())
+
 
 if __name__ == '__main__':
 
