c9c89773210ae4af1001b001636beb21851e914c
StuskoskiHEB <rutkoski.augustus@heb.com>
2018-01-03 15:04:36 -0600

    new dynamo funcs

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ import boto3
 
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 
-table = dynamodb.Table('PythonlearningTable')
+table = dynamodb.Table('Testing')
 
 
 def handler(event, context):
@@ -18,17 +18,26 @@ def handler(event, context):
             'headers': {'Content-Type': 'application/json'}}
 
 
-def getItemsInDynamoTable(event, context):
-
-    response = table.get_item(
-        Key={
-            'UNQIUE_ID': "testing123"
-        }
-    )
+def add_item_to_dynamo_db(event, context):
 
     data = {
-        'output': response['Item']
+        'output': json.loads(event.body)
     }
+
     return {'statusCode': 200,
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json'}}
+
+
+def get_all_items_in_db(event, context):
+
+    response = table.scan()
+
+    data = {
+        'output': response['Items']
+    }
+
+    return {'statusCode': 200,
+            'body': json.dumps(data),
+            'headers': {'Content-Type': 'application/json'}}
+
