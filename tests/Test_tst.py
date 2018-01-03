e1cf0f076197b1a8f15b4e0deb3225eecf077cf5
StuskoskiHEB <rutkoski.augustus@heb.com>
2018-01-03 11:39:14 -0600

    adding more unit tests to pass

--- tests/Test_tst.py
+++ tests/Test_tst.py
@@ -6,8 +6,16 @@ class MyTestCase(unittest.TestCase):
     def test_default_greeting_set(self):
         self.assertEqual(2, (1 + 1))
 
-    def test_fail(self):
-        self.assertTrue('foo'.isupper())
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
 
 
 if __name__ == '__main__':
