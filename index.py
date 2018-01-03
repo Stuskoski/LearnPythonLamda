15fdbd17c0d2e88924756f2045af6a203849509a
StuskoskiHEB <rutkoski.augustus@heb.com>
2018-01-03 13:58:01 -0600

    changed dyanmo url

--- index.py
+++ index.py
@@ -2,9 +2,7 @@ import json
 import datetime
 import boto3
 
-dynamodb = boto3.resource('dynamodb',
-                          region_name='us-east-1',
-                          endpoint_url="http://localhost:8000")
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 
 table = dynamodb.Table('PythonlearningTable')
 
