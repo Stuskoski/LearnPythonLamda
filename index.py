9a1c9cea42a53a25f6901acc21d77822ae5ef5de
StuskoskiHEB <rutkoski.augustus@heb.com>
2018-01-06 23:58:52 -0600

    new header

--- index.py
+++ index.py
@@ -17,6 +17,7 @@ def handler(event, context):
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials' : true,
             'Access-Control-Allow-Headers':'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
             'Access-Control-Allow-Methods': 'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT'}}
 
