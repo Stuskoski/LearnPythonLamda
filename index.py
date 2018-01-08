79e297662613b1d21243d874693ee1dc5afb1975
StuskoskiHEB <rutkoski.augustus@heb.com>
2018-01-08 10:13:22 -0600

    Added cors back

--- index.py
+++ index.py
@@ -15,14 +15,20 @@ def handler(event, context):
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
+            'headers': {'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers':'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+            'Access-Control-Allow-Methods': 'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT'}}
 
 
 def add_item_to_dynamo_db(event, context):
     print(context)
     return {'statusCode': 200,
             'body': json.dumps(event),
-            'headers': {'Content-Type': 'application/json'}}
+            'headers': {'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers':'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+            'Access-Control-Allow-Methods': 'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT'}}
 
 
 def get_all_items_in_db(event, context):
@@ -35,4 +41,7 @@ def get_all_items_in_db(event, context):
 
     return {'statusCode': 200,
             'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
+            'headers': {'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers':'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+            'Access-Control-Allow-Methods': 'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT'}}
